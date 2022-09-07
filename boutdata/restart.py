e04e1f12b16054805ab8511e01fa1d2705e73ef5
John Omotani <john.omotani@ukaea.uk>
2022-09-07 15:17:44 +0100

    Use separate *shift parameter for each dimension
    Accounts for variables that may be staggered to CELL_XLOW, CELL_YLOW, or CELL_ZLOW locations.

--- boutdata/restart.py
+++ boutdata/restart.py
@@ -186,24 +186,26 @@ def resize(
             nx, ny, nz = data.shape
             dx, dy, dz = old['dx'].flat[0], old['dy'].flat[0], old['dz'].flat[0]
             # shift grid if CELL-CENTRED   
-            shift = 0.5 if old.attributes(var)['cell_location'] == 'CELL-CENTRE' else 0
+            xshift = 0.0 if old.attributes(var)['cell_location'] == 'CELL_XLOW' else 0.5
+            yshift = 0.0 if old.attributes(var)['cell_location'] == 'CELL_YLOW' else 0.5
+            zshift = 0.0 if old.attributes(var)['cell_location'] == 'CELL_ZLOW' else 0.5
             
             # Make coordinates
             # NOTE: The max min of the coordinates are irrelevant when
             #       interpolating (as long as old and new coordinates
             #       are consistent), so we just choose all variable to
             #       be between 0 and 1 Calculate the old coordinates 
-            xCoordOld = np.arange(nx - mxg + shift)*dx
-            yCoordOld = np.arange(ny - myg + shift)*dy
-            zCoordOld = np.arange(nz + shift)*dz
+            xCoordOld = np.arange(nx - mxg + xshift)*dx
+            yCoordOld = np.arange(ny - myg + yshift)*dy
+            zCoordOld = np.arange(nz + zshift)*dz
             # Calculate the new spacing
             newDx= dx * ((nx - 2*mxg) / (newNx - 2*mxg) )
             newDy= dy * ((ny - 2*myg) / (newNy - 2*myg) )
             newDz= dz * (nz / newNz )
             # Calculate the new coordinates
-            xCoordNew = np.arange(newNx - mxg + shift)*newDx
-            yCoordNew = np.arange(newNy - myg + shift)*newDy
-            zCoordNew = np.arange(newNz + shift)*newDz
+            xCoordNew = np.arange(newNx - mxg + xshift)*newDx
+            yCoordNew = np.arange(newNy - myg + yshift)*newDy
+            zCoordNew = np.arange(newNz + zshift)*newDz
 
             # Make a pool of workers
             pool = multiprocessing.Pool(maxProc)
