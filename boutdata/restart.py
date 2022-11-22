b98f7357e0ebca1833561a5db0cd2615ef086467
Michael Redenti <43955379+mredenti@users.noreply.github.com>
2022-11-22 23:42:18 +0000

    Do not raise an error if grid points to be interpolated outside range and instead extrapolate by setting fill_value=None

--- boutdata/restart.py
+++ boutdata/restart.py
@@ -69,7 +69,7 @@ def resize3DField(var, data, coordsAndSizesTuple, method, mute):
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.RegularGridInterpolator.html
     # for details)
     gridInterpolator = RegularGridInterpolator(
-        (xCoordOld, yCoordOld, zCoordOld), data, method, fill_value = 0.0
+        (xCoordOld, yCoordOld, zCoordOld), data, method, bounds_error = False, fill_value = None
     )
 
     # Need to fill with one exrta z plane (will only contain zeros)
