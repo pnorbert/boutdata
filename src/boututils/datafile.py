7c97f98d9fe0e8f7d65e2d7671c7e1d866cfc17d
Norbert Podhorszki <pnorbert@ornl.gov>
2024-08-08 15:01:51 -0400

    fix syntax error and remove useless ".bp5" extension from supported list. Only ".bp" matters.

--- src/boututils/datafile.py
+++ src/boututils/datafile.py
@@ -103,7 +103,7 @@ class DataFile(object):
                 self.impl = DataFile_HDF5(
                     filename=filename, write=write, create=create, format=format
                 )
-            elif filename.split(".")[-1] in ("bp", "bp5"):
+            elif filename.split(".")[-1] in ("bp"):
                 self.impl = DataFile_ADIOS2(
                     filename=filename, write=write, create=create, format=format
                 )
@@ -119,7 +119,7 @@ class DataFile(object):
             self.impl = DataFile_HDF5(
                 filename=filename, write=write, create=create, format=format
             )
-        elif format.lower().startswith() == "adios":
+        elif format.lower().startswith("adios"):
             self.impl = DataFile_ADIOS2(
                 filename=filename, write=write, create=create, format=format
             )
